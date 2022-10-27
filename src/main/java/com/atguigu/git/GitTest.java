a0625e89e0a4871b34ebebd4864f0bf3f73576d0
luohengfeng <lhf@atguigu.com>
2022-10-27 12:01:36 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 22509
+ * @create 2022-10-27 11:52
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
